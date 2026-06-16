b6b42b5fc5146f6ac4616126c95e8434233c349b
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-16 22:09:48 +0500

    GameStore

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router"
+
+
+const App = () => {
+  const router = createBrowserRouter([
+
+
+  ])
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default App
---
+++ src/lib/api/games.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const api = "https://69e5ff70ce4e908a155ec5a1.mockapi.io/users";
+
+export const getUsers = async () => {
+  const res = await axios.get(api);
+  return res.data;
+};
+
+export const deleteUser = async (id) => {
+  return await axios.delete(`${api}/${id}`);
+};
+
+export const createUser = async (user) => {
+  return await axios.post(api, user);
+};
