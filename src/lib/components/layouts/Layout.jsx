fc934fbc7e8df0694f67c9b018f530ce83ecb49b
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-24 22:47:03 +0500

    Link

--- src/lib/components/layouts/Layout.jsx
+++ src/lib/components/layouts/Layout.jsx
@@ -1,11 +1,21 @@
+import { Link } from "react-router";
 import logo from "../../../assets/magmus.png"
 
 const Layout = () => {
   return (
-    <div className="w-full">
-        <div><img src={logo} alt="" className="w-15 rounded-3xl" /></div>
+    <div className="w-full h-18 bg-neutral-300 flex justify-between items-center">
+      <div className=" flex justify-center items-center gap-5">
+        <img src={logo} alt="" className="w-15 rounded-2xl" />
+        <p className=" text-xl font-bold">MagMus® Store</p>
+      </div>
+      <div>
+        <Link to="/">Главная</Link>
+        <Link to="/">Игры</Link>
+        <Link to="/">О нас</Link>
+      </div>
+      
     </div>
-  )
+  );
 }
 
 export default Layout
