5c156ce5bb151fbd0d64b924eb3e03308d3a93b6
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-24 23:58:08 +0500

    home

--- src/lib/components/layouts/home.jsx
+++ src/lib/components/layouts/home.jsx
@@ -7,9 +7,15 @@ import "swiper/css/navigation";
 
 
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { useStore } from "../../store/store";
+import { useEffect } from "react";
+import { getGames } from "../../api/games";
 
 const Home = () => {
-
+const {games, setGames} = useStore();
+useEffect(()=>{
+    const reGames = getGames
+})
 
   return (
     <>
@@ -30,13 +36,6 @@ const Home = () => {
       >
         <SwiperSlide>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
     </>
   );
