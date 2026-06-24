f062749a60cf4ca9d9fc74f608a2a90bbc49822d
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-24 22:05:19 +0500

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router"
+import Layout from "./lib/components/layouts/Layout"
+import Home from "./lib/components/layouts/home"
 
 
 const App = () => {
   const router = createBrowserRouter([
+    {
+    path:"/",
+    element: <Layout/>,
+    children:[{
+      path:"/",
+      index: true,
+      element: <Home/>, 
+    }]
+
+    }
 
 
   ])
