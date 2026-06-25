5ac18712d48e978320e16ba9ee11c6c28c7e8b55
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-25 22:22:33 +0500

    logo

--- src/lib/components/prodByID.jsx
+++ src/lib/components/prodByID.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router"; 
+import { useParams } from "react-router";
 import { useQuery } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
 import { getGamesByID } from "../api/games";
@@ -6,22 +6,15 @@ import { useState } from "react";
 import ReactPlayer from "react-player";
 import { Button } from "antd";
 
-
-
-
-
 const ProdByID = () => {
-    const [isPlaying, setIsPlaying] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   const { id } = useParams();
 
-  const {
-    data: game,
-    isLoading
-  } = useQuery({
+  const { data: game, isLoading } = useQuery({
     queryKey: ["game", id],
-    queryFn: () => getGamesByID(id), 
-    enabled: !!id, 
+    queryFn: () => getGamesByID(id),
+    enabled: !!id,
   });
 
   if (isLoading) {
@@ -32,26 +25,26 @@ const ProdByID = () => {
     );
   }
 
-
-
   return (
     <div className="p-20 text-white">
       <h1 className="text-4xl font-bold mb-5 ">{game.name}</h1>
       <p className="text-green-700 font-bold text-2xl ">$ {game.price} USD</p>
-      <div className=" flex justify-between items-start">
+      <div className=" flex justify-between items-center mt-5">
         <ReactPlayer
           src={game.trailer}
           controls
-          width="
-      60%"
+          width="60%"
           height="500px"
         />
-        <div>
-          <img src={game.logo} alt="" className="w-68" />
-          <div className="w-full h-0.5 bg-neutral-400"></div>
-<p>{game.title}</p>
-          <div className="w-full h-0.5 bg-neutral-400"></div>
-          <Button>Buy Now</Button>
+        <div className="w-[35%] flex justify-center items-center flex-col">
+          <img src={game.logo} alt="" className="w-full h-35 object-cover" />
+          <div className="w-full h-0.5 bg-neutral-400 mt-10"></div>
+          <p className="h-40 pt-2">{game.title}</p>
+          <div className="w-full h-0.5 bg-neutral-400 mt-5"></div>
+          <Button className="mt-5! w-60! rounded-2xl!">Buy Now</Button>
+          <Button className="mt-5! w-60! rounded-2xl! bg-blue-950! border-0! text-white!">
+            Add to Cart
+          </Button>
         </div>
       </div>
     </div>
