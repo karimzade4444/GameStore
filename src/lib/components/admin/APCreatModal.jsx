08525e206c4a7215293a5c4f3da016061b064e11
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-27 00:05:48 +0500

    Modal

--- src/lib/components/admin/APCreatModal.jsx
+++ src/lib/components/admin/APCreatModal.jsx
@@ -1,9 +1,59 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Modal } from "antd";
+import { useState } from "react";
+import { createGames } from "../../api/games";
+import { useStore } from "../../store/store";
 
 
 const APCreatModal = () => {
+    const [createModal, setCreateModal] = useState(false);
+    const queryClient = useQueryClient();
+
+     const { mutate, isPending } = useMutation({
+       mutationFn: createGames,
+       onSuccess: () => {
+         void queryClient.invalidateQueries({ queryKey: ["games"] });
+         setCreateModal(false);
+       },
+     });
+
+     const name = useStore((state)=>state.name);
+     const img = useStore((state) => state.img);
+
+     const price = useStore((state) => state.price);
+     const logo = useStore((state) => state.logo);
+     const name = useStore((state) => state.name);
+     const name = useStore((state) => state.name);
+     const name = useStore((state) => state.name);
+     const name = useStore((state) => state.name);
+     const name = useStore((state) => state.name);
+     const name = useStore((state) => state.name);
+     const name = useStore((state) => state.name);
+     const name = useStore((state) => state.name);
+     const name = useStore((state) => state.name);
+
+
+
   return (
-    <div>APCreatModal</div>
-  )
+    <>
+      <Modal
+        title="Basic Modal"
+        closable={{ "aria-label": "Custom Close Button" }}
+        open={createModal}
+        onCancel={() => setCreateModal(false)}
+        onOk={() => {
+          mutate({
+            name,
+            title,
+            category,
+          });
+        }}
+        confirmLoading={isPending}
+      >
+      
+      </Modal>
+    </>
+  );
 }
 
 export default APCreatModal
