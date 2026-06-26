fa31f6778e24f2e846a6f4c625ac05ac8f370207
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-26 22:51:13 +0500

    left

--- src/lib/components/admin/adminPanel.jsx
+++ src/lib/components/admin/adminPanel.jsx
@@ -1,12 +1,28 @@
-
+import { SquareLibrary } from "lucide-react";
+import logo from "../../../assets/magmus.png"
 
 const AdminPanel = () => {
   return (
     <div className=" flex justify-between items-center">
-        <div className="w-[20%] bg-gray-900 min-h-screen border-3 border-black"> </div>
-        <div className="w-[80%] min-h-screen bg-linear-to-r from-blue-800/40 to-gray-900 "></div>
+      <div className="w-[20%] bg-gray-900 min-h-screen border-3 border-black">
+        <div className=" flex justify-center items-center gap-5 mt-7">
+          <img
+            src={logo}
+            alt=""
+            className="w-15 rounded-2xl shadow-2xl shadow-white/60"
+          />
+          <p className=" text-2xl font-medium text-white ">MagMus® Store</p>
+        </div>
+        <div className=" p-10">
+          <div className=" flex justify-start items-center gap-5 border-3 p-2 rounded-xl border-white/50 bg-linear-to-r from-blue-700/50 via-purple-600/50 to-rose-500/50 cursor-pointer hover:bg-neutral-600">
+            <SquareLibrary color="white" size={48} />
+            <p className=" text-2xl font-medium text-white">Library</p>
+          </div>
+        </div>
+      </div>
+      <div className="w-[80%] min-h-screen bg-linear-to-r from-blue-800/40 to-gray-900 "></div>
     </div>
-  )
+  );
 }
 
 export default AdminPanel
