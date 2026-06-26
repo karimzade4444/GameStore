7323a457f6928637b04514ad12439e498df77de3
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-26 22:12:07 +0500

    bg admin

--- src/lib/components/admin/adminPanel.jsx
+++ src/lib/components/admin/adminPanel.jsx
@@ -2,7 +2,10 @@
 
 const AdminPanel = () => {
   return (
-    <div>AdminPanel</div>
+    <div className=" flex justify-between items-center">
+        <div className="w-[20%] bg-gray-900 min-h-screen border-3 border-black"> </div>
+        <div className="w-[80%] min-h-screen bg-linear-to-r from-blue-800/40 to-gray-900 "></div>
+    </div>
   )
 }
 
